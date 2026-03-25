89b3cf1891291995df3ba951ab83cff107f19d89
CheHyeonYeong <gusdudco6@naver.com>
2026-03-25 11:43:08 +0900

    feat: Chapter 도메인 구현
    - Chapter 엔티티 (단원 정보)
    - ChapterRepository
    - ChapterService (조회, 존재 검증)

---
+++ src/main/java/com/teamsky/learning/chapter/ChapterRepository.java
@@ -0,0 +1,7 @@
+package com.teamsky.learning.chapter;
+
+import com.teamsky.learning.chapter.entity.Chapter;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChapterRepository extends JpaRepository<Chapter, Long> {
+}
---
+++ src/main/java/com/teamsky/learning/chapter/entity/Chapter.java
@@ -0,0 +1,35 @@
+package com.teamsky.learning.chapter.entity;
+
+import com.teamsky.learning.common.entity.BaseTimeEntity;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "chapters")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Chapter extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 200)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private Integer orderNum;
+
+    @Builder
+    public Chapter(String name, String description, Integer orderNum) {
+        this.name = name;
+        this.description = description;
+        this.orderNum = orderNum;
+    }
+}
